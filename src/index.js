2dd850d750dd8019b3a7c0bee319808252e6db83
Sarthak Shinde <srthak007@gmail.com>
2022-07-01 11:52:56 +0530

    set env commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const Song = require("./songSchema");
 const multer = require("multer");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 
